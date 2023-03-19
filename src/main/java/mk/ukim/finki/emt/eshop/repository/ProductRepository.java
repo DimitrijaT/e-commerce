2ebf514feea72fbdd8f6756933e61add70643112
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-03-20 00:10:05 +0100

    Initial Project, FetchType, EntityGraph, JPA Projections

---
+++ src/main/java/mk/ukim/finki/emt/eshop/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package mk.ukim.finki.emt.eshop.repository;
+
+import mk.ukim.finki.emt.eshop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+//    Optional<Product> findByNameAndByPriceLessThan(String name, Double price);
+    void deleteByName(String name);
+}
