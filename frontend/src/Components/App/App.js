51576f65e38925646be31557c72688904ade7122
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-03-23 22:54:41 +0100

    Intro to React, Creating React app, Statefull and Stateless components, Information transfer between components, Lifecycle of a component, Axios, Dealing with CORS error, Bootstrap integration

---
+++ frontend/src/Components/App/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, {Component} from "react";
+import Manufacturers from "../Manufacturers/manufacturers";
+import EShopService from "../../repository/eshopRepository";
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            manufacturers: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Manufacturers manufacturers={this.state.manufacturers}/>
+            </div>
+        );
+    }
+
+    loadManufacturers = () => {
+        EShopService.fetchManufacturers()
+            .then((data) => {
+                this.setState({
+                    manufacturers: data.data
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.loadManufacturers();
+    }
+
+}
+
+export default App;
---
+++ frontend/src/custom-axios/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// Here we will set up everything necessary for axios to make
+// the real calls and not make these calls multiple times
+
+const instance = axios.create({
+    baseURL: 'http://localhost:9091/api',
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+})
+
+//We will use this instance for all parts of our React app
+export default instance;
+
