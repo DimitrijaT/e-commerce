ef688997c6423d61b17928d6c1cf3a1561084750
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-03-24 00:10:22 +0100

    React navigation

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -1,23 +1,41 @@
 import './App.css';
 import React, {Component} from "react";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import Manufacturers from "../Manufacturers/manufacturers";
 import EShopService from "../../repository/eshopRepository";
+import Products from "../Products/ProductList/products";
+import Categories from "../Categories/categories";
+import Header from "../Header/header";
 
-class App extends Component{
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            manufacturers: []
+            manufacturers: [],
+            products: [],
+            categories: []
         }
     }
 
     render() {
         return (
-            <div>
-                <Manufacturers manufacturers={this.state.manufacturers}/>
-            </div>
+            <Router>
+                <Header/>
+                <main>
+                    <header/>
+                    <div className="container">
+                        <Routes>
+                            <Route path={"/manufacturers"} element={<Manufacturers manufacturers={this.state.manufacturers}/>}/>
+                            <Route path={"/products"} element={<Products products={this.state.products}/>}/>
+                            <Route path={"/categories"} element={<Categories categories={this.state.categories}/>}/>
+                            <Route path={"*"} element={<Navigate to={"/products"} replace={true}/>}/>
+                        </Routes>
+                    </div>
+                </main>
+            </Router>
         );
     }
+
 
     loadManufacturers = () => {
         EShopService.fetchManufacturers()
@@ -28,8 +46,28 @@ class App extends Component{
             });
     }
 
+    loadProducts = () => {
+        EShopService.fetchProducts()
+            .then((data) => {
+                this.setState({
+                    products: data.data
+                })
+            });
+    }
+
+    loadCategories = () => {
+        EShopService.fetchCategories()
+            .then((data) => {
+                this.setState({
+                    categories: data.data
+                })
+            });
+    }
+
     componentDidMount() {
         this.loadManufacturers();
+        this.loadProducts();
+        this.loadCategories();
     }
 
 }
--- frontend/src/repository/eshopRepository.js
+++ frontend/src/repository/eshopRepository.js
@@ -3,6 +3,12 @@ import axios from '../custom-axios/axios'
 const EShopService = {
     fetchManufacturers: () => {
         return axios.get("/manufacturers");
+    },
+    fetchProducts: () => {
+        return axios.get("/products");
+    },
+    fetchCategories: () => {
+        return axios.get("/categories");
     }
 }
 
